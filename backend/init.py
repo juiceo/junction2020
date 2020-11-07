141be8c8c6f4d67d09c747be3cdc19605035787f
Kenneth Blomqvist <kekeblom@gmail.com>
2020-11-08 00:15:55 +0100

    COrs via flask_cors

--- backend/init.py
+++ backend/init.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 db = SQLAlchemy()
 def create_app():
@@ -9,3 +10,5 @@ def create_app():
     return app
 
 app = create_app()
+CORS(app)
+
