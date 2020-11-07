346f1ce9daae4094d58008a4755b1d9939437bab
Martta Jämsén <marttajamsen@Martta-MacBook-Pro.local>
2020-11-07 15:11:19 +0200

    sds

--- dashboard/src/data/expenses.ts
+++ dashboard/src/data/expenses.ts
@@ -1,4 +1,8 @@
-export type Category = 'Food' | 'Transport';
+export type Category = 'Food' | 'Transport'| 'Travel'|'Sports'|'Living'|'Streaming'|'Entertainment'
+
+
+
+
 
 export type Expense = {
     label: string;
@@ -19,12 +23,32 @@ export type FixedExpense = {
 
 export const expenses_daily_random: Expense[] = [
     {
-        label: 'Groceries',
+        label: 'Coffee',
         category: 'Food',
     },
     {
         label: 'Groceries',
         category: 'Food',
+    },
+    {
+        label: 'Lunch',
+        category: 'Food',
+    },
+    {
+        label: 'Taxi',
+        category: 'Transport',
+    },
+    {
+        label: 'Gas',
+        category: 'Transport',
+    },
+    {
+        label: 'Parking ticket',
+        category: 'Transport',
+    },
+    {
+        label: 'Tennis',
+        category: 'Sports',
     },
 ];
 
@@ -37,11 +61,51 @@ export const expenses_monthly_random: Expense[] = [
         label: 'Flight tickets',
         category: 'Travel',
     },
+    {
+        label: 'Hotel',
+        category: 'Travel',
+    },
+    {
+        label: 'Dinner',
+        category: 'Entertainment',
+    },
 ];
 
 export const expenses_monthly_fixed: Expense[] = [
     {
         label: 'Public transport ticket',
         category: 'Transport',
+    },
+    {
+        label: 'Rent',
+        category: 'Living',
+    },
+    {
+        label: 'Netflix',
+        category: 'Streaming',
+    },
+    {
+        label: 'Phone bill',
+        category: 'Living',
+    },
+    {
+        label: 'Electricity',
+        category: 'Living',
+    },
+    {
+        label: 'Home insurance',
+        category: 'Living',
+    },
+    {
+        label: 'Spotify',
+        category: 'Streaming',
+    },
+    {
+        label: 'Gym membership',
+        category: 'Sports',
+    },
+    {
+        label: 'Internet',
+        category: 'Living',
     },
 ];
