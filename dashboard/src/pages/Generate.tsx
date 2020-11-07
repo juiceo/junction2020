da2f03f923bb983c88703e91e18e7d71c2e08d8b
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2020-11-07 16:24:12 +0200

    add profiles to gen

--- dashboard/src/pages/Generate.tsx
+++ dashboard/src/pages/Generate.tsx
@@ -13,11 +13,13 @@ import {
 } from '@material-ui/core';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import { groupBy } from 'lodash';
+
 import moment from 'moment';
+import { sumBy } from 'lodash';
 
 import { ExpenseTransaction, generateExpenses } from '../data/generateExpenses';
 import { SpenderProfile } from '../data/profiles';
-import { student } from '../data/profiles';
+import { student, constructionWorker, instagramInfluencer, businessPerson } from '../data/profiles';
 
 interface Props {}
 const Generate = (props: Props) => {
@@ -35,22 +37,23 @@ const Generate = (props: Props) => {
             <Button color="primary" variant="contained" onClick={() => handleGenerateExpenses(student)}>
                 Student
             </Button>
-            <Button color="primary" variant="contained" onClick={() => handleGenerateExpenses(student)}>
-                Profile 2
+            <Button color="primary" variant="contained" onClick={() => handleGenerateExpenses(constructionWorker)}>
+                Construction worker
             </Button>
-            <Button color="primary" variant="contained" onClick={() => handleGenerateExpenses(student)}>
-                Profile 3
+            <Button color="primary" variant="contained" onClick={() => handleGenerateExpenses(instagramInfluencer)}>
+                Instagram influencer
             </Button>
-            <Button color="primary" variant="contained" onClick={() => handleGenerateExpenses(student)}>
-                Profile 4
+            <Button color="primary" variant="contained" onClick={() => handleGenerateExpenses(businessPerson)}>
+                Business person
             </Button>
             <Box p={2}>
                 {Object.keys(expensesByMonth).map((month) => {
                     const expenses = expensesByMonth[month];
+                    const total = sumBy(expenses, (e) => Number(e.amount));
                     return (
                         <Box mt={3}>
                             <Typography variant="h4" gutterBottom>
-                                {month}
+                                {month} - {total} EUR
                             </Typography>
                             <TableContainer component={Paper}>
                                 <Table>
