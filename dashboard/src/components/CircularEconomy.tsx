b554527ab97b2e2ce4fb312c8f6544d4b8b019ad
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2020-11-07 21:48:47 +0200

    add circular economy card

--- dashboard/src/components/CircularEconomy.tsx
+++ dashboard/src/components/CircularEconomy.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { Paper, Box, Typography, Button } from '@material-ui/core';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 
-import SwapHorizIcon from '@material-ui/icons/Whatshot';
+import SwapHorizIcon from '@material-ui/icons/SwapHoriz';
+import image from '../assets/autofilter.png';
 
 interface Props {
     onAccept: () => void;
@@ -14,40 +15,28 @@ const CircularEconomy = (props: Props) => {
 
     return (
         <Paper>
-            <Box p={2}>
-                <Box className={classes.tag} mb={3}>
-                    <SwapHorizIcon /> <Box mr={1} /> Circular economy
-                </Box>
-                <Typography variant="body1">Rent your tools</Typography>
-                <Typography variant="body2">
-                    Do you have some tools you can set for rent? Here are a few people in your neighborhood looking to
-                    rent some tools:
-                </Typography>
-                <Box display="flex" flexDirection="row" my={4}>
-                    <Box
-                        className={classes.red}
-                        flex={1}
-                        display="flex"
-                        flexDirection="column"
-                        alignItems="center"
-                        justifyContent="center"
-                    >
-                        <Typography variant="h3">1235,90€</Typography>
-                        <Typography variant="body1">You</Typography>
-                    </Box>
-                    <Box flex={1} display="flex" flexDirection="column" alignItems="center" justifyContent="center">
-                        <Typography variant="h3">675,09€</Typography>
-                        <Typography variant="body1">Average</Typography>
+            <Box>
+                <Box p={2} pb={0}>
+                    <Box className={classes.tag} mb={3}>
+                        <SwapHorizIcon /> <Box mr={1} /> Circular economy
                     </Box>
                 </Box>
-                <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center">
-                    <Button variant="outlined" onClick={props.onAccept}>
-                        Rent your stuff
-                    </Button>
-                    <Box mt={3} />
-                    <Button size="small" onClick={props.onSkip}>
-                        Next suggestion
-                    </Button>
+                <img src={image} alt="" className={classes.image} />
+                <Box p={2}>
+                    <Typography variant="body1">Rent your tools</Typography>
+                    <Typography variant="body2">
+                        Do you have some tools you can set for rent? Here are a few people in your neighborhood looking
+                        to rent some tools:
+                    </Typography>
+                    <Box mt={3} display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+                        <Button variant="outlined" onClick={props.onAccept}>
+                            Rent your stuff
+                        </Button>
+                        <Box mt={3} />
+                        <Button size="small" onClick={props.onSkip}>
+                            Next suggestion
+                        </Button>
+                    </Box>
                 </Box>
             </Box>
         </Paper>
@@ -63,6 +52,11 @@ const useStyles = makeStyles((theme: Theme) =>
             display: 'flex',
             flexDirection: 'row',
             alignItems: 'center',
+        },
+        image: {
+            width: '100%',
+            height: 300,
+            objectFit: 'cover',
         },
     })
 );
