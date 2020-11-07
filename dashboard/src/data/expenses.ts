41a25aad17d8605125be555e7b59e90fe397f5a3
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2020-11-07 14:51:40 +0200

    add expense types

--- dashboard/src/data/expenses.ts
+++ dashboard/src/data/expenses.ts
@@ -1,4 +1,21 @@
-import { Expense } from './expenseTypes';
+export type Category = 'Food' | 'Transport';
+
+export type Expense = {
+    label: string;
+    category: Category;
+};
+
+export type RandomExpense = {
+    label: string;
+    min: number;
+    max: number;
+    probability: number;
+};
+
+export type FixedExpense = {
+    label: string;
+    amount: number;
+};
 
 export const expenses_daily_random: Expense[] = [
     {
